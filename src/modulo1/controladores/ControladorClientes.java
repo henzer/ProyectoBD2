0384ffeaabc3e3449b9527101c092ad683c0a719
Frecho12540 <mar12540@uvg.edu.gt>
2015-05-23 23:05:45 -0600

    Se agregaro los metodos para Agregar campo, Actualizar campos, Crear un nuevo cliente y Eliminar un cliente.

--- src/modulo1/controladores/ControladorClientes.java
+++ src/modulo1/controladores/ControladorClientes.java
@@ -11,46 +11,58 @@ import org.json.JSONObject;
 import modulo1.conexion.ConexionPostgres;
 import modulo2.conexion.Conexion;
 
-public class ControladorClientes {
+public class ControladorClientes
+{
 	private JSONArray dataActual;
 	private int posActual;
 	
 	private static ControladorClientes instancia;
-	public static ControladorClientes getInstancia(){
+	public static ControladorClientes getInstancia()
+	{
 		if(instancia==null)
 			instancia = new ControladorClientes();
 		return instancia;
 	}
 	
-	public ControladorClientes(){
+	public ControladorClientes()
+	{
 		dataActual = new JSONArray();
 	}
 	
 	//
-	public DefaultTableModel getDataClientes(List<String> columnas, String query){
+	public DefaultTableModel getDataClientes(List<String> columnas, String query)
+	{
 		DefaultTableModel model = new DefaultTableModel();
 		dataActual = ConexionPostgres.getInstancia().executeQuery(query);
-		for(String header: columnas){
+		for(String header: columnas)
+		{
 			model.addColumn(header);
 		}
-		try {
-			for(int i=0; i<dataActual.length(); i++){
+		try 
+		{
+			for(int i=0; i<dataActual.length(); i++)
+			{
 				JSONObject elemento = (JSONObject)dataActual.get(i);
 				Object [] row = new Object[columnas.size()];
-				for(int j=0; j<columnas.size(); j++){
+				for(int j=0; j<columnas.size(); j++)
+				{
 					row[j] = elemento.get(columnas.get(j));
 				}
 				model.addRow(row);
 			}
-		} catch (JSONException e) {
+		}
+		catch (JSONException e)
+		{
 			e.printStackTrace();
 		}
 		return model;
 	}
 	
 	//Devuelve el primer elemento de la lista actual. Si esta vacia retorna NULL
-	public JSONObject getFirst() throws Exception{
-		if(dataActual.length()>0){
+	public JSONObject getFirst() throws Exception
+	{
+		if(dataActual.length()>0)
+		{
 			posActual = 0;
 			return (JSONObject) dataActual.get(0);
 		}
@@ -58,8 +70,10 @@ public class ControladorClientes {
 	}
 	
 	//Devuelve el ultimo elemento de la lista actual. Si esta vacia retorna NULL
-	public JSONObject getLast() throws Exception{
-		if(dataActual.length()>0){
+	public JSONObject getLast() throws Exception
+	{
+		if(dataActual.length()>0)
+		{
 			posActual = dataActual.length()-1;
 			return (JSONObject) dataActual.get(dataActual.length()-1);
 		}
@@ -67,13 +81,15 @@ public class ControladorClientes {
 	}
 	
 	//Devuelve el elemento ubicado en la posición POS, si está vacia retorna null.
-	public JSONObject getElement(int pos) throws Exception{
+	public JSONObject getElement(int pos) throws Exception
+	{
 		if(pos<0)
 			pos = 0;
 		if(pos>dataActual.length()-1)
 			pos = dataActual.length()-1;
 			
-		if(dataActual.length()>0){
+		if(dataActual.length()>0)
+		{
 			posActual = pos;
 			return (JSONObject) dataActual.get(pos);
 		}
@@ -82,18 +98,21 @@ public class ControladorClientes {
 	
 	//Inserta una nueva columna. Lanza una Excepción si se intenta crear una columna que ya existe.
 	//Cualquier otro error no lo maneja, simplemente lanzaría false. (Se debe manejar desde donde se llame).
-	public boolean insertNewRow(String name, String type, String _default) throws Exception{
+	public boolean insertNewRow(String name, String type, String _default) throws Exception
+	{
 		List<String> columns = ConexionPostgres.getInstancia().getTableColumns("clientes");
 		if(columns.contains(name))
 			throw new Exception("ERROR.-Ya existe una columna con ese nombre");
 		return ConexionPostgres.getInstancia().executeUpdate("ALTER TABLE clientes ADD COLUMN "+name+" "+ type +" DEFAULT "+_default+";");		
 	}
 
-	public int getPosActual() {
+	public int getPosActual()
+	{
 		return posActual;
 	}
 
-	public void setPosActual(int posActual) {
+	public void setPosActual(int posActual) 
+	{
 		this.posActual = posActual;
 	}
 }
